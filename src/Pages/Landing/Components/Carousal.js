6c51247d5ff721a0bb3e95e986066d67d6118f2e
Eyob Samuel <leeyoba@gmail.com>
2021-06-15 20:19:38 +0300

    Line height

--- src/Pages/Landing/Components/Carousal.js
+++ src/Pages/Landing/Components/Carousal.js
@@ -81,6 +81,7 @@ export default function Carousal() {
                       background: "#728C99",
                       display: "inline",
                       color: "white",
+                      lineHeight: "50px",
                     }}
                   >
                     {i.title}
