dddeb4c5d0d7da7f52a5232d681ab0e5b0b74939
Eyob Samuel <leeyoba@gmail.com>
2021-06-16 18:48:57 +0300

    Title is modified

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -81,7 +81,7 @@ export default function Home() {
               <div className="sm:text-center lg:text-left">
                 <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                   <span className="pb-4 text-indigo-600 block xl:inline text-center">
-                    TenaAdam
+                    TennaAdam
                   </span>{" "}
                 </h1>
                 {/* <p className="mt-3 text-center text-base text-gray-500 sm:mt-5 text-2xl sm:max-w-xl sm:mx-auto md:mt-5 lg:mx-0"> */}
